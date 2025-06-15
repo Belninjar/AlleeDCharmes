03baf2ae78f065f74e08267cbce8c98f6a4fce57
Belninjar <95588496+Belninjar@users.noreply.github.com>
2025-06-15 19:24:07 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// === Configuration Supabase ===
+// Remplace ces deux valeurs par celles de ton tableau de bord (Settings → API)
+export const SUPABASE_URL = 'https://qadqmeerruivexijftxf.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFhZHFtZWVycnVpdmV4aWpmdHhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwMDU4MDEsImV4cCI6MjA2NTU4MTgwMX0.y_MLb1Zl4eQsnRNqF48q6_hDtl2Fu2hV36OLaLyX4iw';
